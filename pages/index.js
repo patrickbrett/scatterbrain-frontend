ee26e427e45bf3c6919b15e3f2cc315aa18b16e8
Patrick Brett <patrick@livebook.io>
2020-08-08 17:07:03 +1000

    Player refresh

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import SocketClient from '../lib/SocketClient';
 import Link from "next/link";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
--- pages/play/[gameCode]/[playerCode].js
+++ pages/play/[gameCode]/[playerCode].js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
+import { withRouter } from "next/router";
+import PlayerManager from "../../../lib/PlayerManager";
 
-export default class PlayGame extends Component {
+class PlayGame extends Component {
+  state = {
+    playerName: null,
+    isVip: null
+  }
+
+  componentDidMount() {
+    const playerManager = PlayerManager.getInstance();
+    playerManager.setSocketListener(this.socketListener);
+
+    if (playerManager.gameCode) {
+      this.setState({ playerName: playerManager.playerName, isVip: playerManager.isVip });
+    }
+  }
+
+  componentDidUpdate() {
+    const { gameCode, playerCode } = this.props.router.query;
+
+    if (!gameCode || !playerCode) return;
+
+    const playerManager = PlayerManager.getInstance();
+    if (!playerManager.gameCode) { // player manager has lost game info, get it back
+      playerManager.refetchGameInfo(gameCode, playerCode);
+    }
+  }
+
+  socketListener = (data) => {
+    const { event } = data;
+    if (event === 'reload-player-accepted') {
+      const { playerName, isVip } = data;
+      this.setState({ playerName, isVip })
+    }
+  }
+
   render() {
-    return (<div>Playing game</div>);
+    const { gameCode } = this.props.router.query;
+    const { playerName, isVip } = this.state;
+
+    return (
+      <div>
+        <div>Playing game</div>
+        <div>Game code: {gameCode}</div>
+        <div>Player name: {playerName} {isVip ? '(VIP)' : null}</div>
+      </div>
+    );
   }
 }
+
+export default withRouter(PlayGame);
