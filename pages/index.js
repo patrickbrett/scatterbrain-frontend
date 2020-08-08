a6325ffdd5d76cb858883bab31ba624c99f57908
Patrick Brett <patrick@livebook.io>
2020-08-08 18:36:52 +1000

    Add title

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <h1>Scatterbrain.tv</h1>
+      <div>Categories game online</div>
       <Link href="/host"><a href="/host">Host game</a></Link>
       <Link href="/play"><a href="/play">Join game</a></Link>
     </div>
